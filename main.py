5e4194d30b289666f8fde70bfa431842990ce025
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-01-30 19:36:20 +0800

    layout update

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ def detect_submit(Model, Epochs, Input_Size, Exports):
     if Task == "Detect":
         model = YOLO(Model + '.yaml')
         model.train(data='datasets/' + Selected_Dataset + '/data.yaml', epochs=int(Epochs), imgsz=int(Input_Size))
-        print(Exports)
-        return Model + " (Detect) training successed."
+        output_path = model.export()
+        results_dir = output_path.split("weights")[0]
+        images = [(results_dir + '/' + filename, filename.split('.')[0]) for filename in [f for f in os.listdir(results_dir)] if '.jpg' in filename or  '.png' in filename]
+        return images, Model + " (Detect) training successed."
     else:
-        return Model + " (Detect) are not support given Dataset."
+        return None, Model + " (Detect) are not support given Dataset."
 
 global Selected_Dataset, Task
 Selected_Dataset, Task = None, None
@@ -28,13 +30,13 @@ detector = gr.Interface(detect_submit,
                                           gr.Slider(240, 2048, value=640, label="Input_Size", info=""),
                                           gr.CheckboxGroup(["PyTorch", "TorchScript", "ONNX", "OpenVINO", "TensorRT", "CoreML", "TF SavedModel", "TF Lite", "TF Edge TPU", "TF.js", "PaddlePaddle", "ncnn"], label="Exports", info=""),
                                       ],
-                             outputs = gr.Textbox(),
+                             outputs = [gr.Gallery(preview=True), gr.Textbox()],
                              allow_flagging="never",
 )
 
 with gr.Blocks() as Ultralytics:
-    gr.Markdown("<span style='font-size:28px; font-weight:bold;'>Hi User, Welcome to ITRI Microservice.</span><span style='font-size:20px; font-weight:bold; color:gray;'>(powered by Ultralytics)</span>")
-    gallery = gr.Gallery(label="Generated images", show_label=False, elem_id="gallery", columns=[10], rows=[4], object_fit="contain", height="auto")
+    gr.Markdown("<span style='font-size:28px; font-weight:bold;'>Hi User, Welcome to ITRI Microservice  </span><span style='font-size:20px; font-weight:bold; color:gray;'>(powered by Ultralytics)</span>")
+    gallery = gr.Gallery(label="Generated images", show_label=False, elem_id="gallery", columns=[10], rows=[3], object_fit="contain", height="auto")
     Dataset = gr.Dropdown(["HardHat"], label="Dataset", info="")
     btn = gr.Button("Load Dataset")
     btn.click(detect_load, Dataset, gallery)
